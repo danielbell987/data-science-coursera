6d2bb0386053aa9b301da23f338802c4c82c89b3
Daniel Bell <danielbell987@gmail.com>
2015-04-06 14:37:07 +0100

    commit

--- Getting and Cleaning Data/Quizzes/Quiz3.R
+++ Getting and Cleaning Data/Quizzes/Quiz3.R
@@ -3,42 +3,47 @@
 ## Quiz 3
 
 ## Question 1
-download.file(url = "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Fss06hid.csv" , destfile = "community.csv")
-
+url <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Fss06hid.csv"
+file <- "community.csv"
+download.file(url = url , destfile = file)
 dat <- read.csv(file = "community.csv")
 
 dat$agricultureLogical <- dat$ACR==3 & dat$AGS==6
-
 which(dat$agricultureLogical)[1:3]
+
 
 ## Question 2
 library(jpeg)
-
-download.file(url = "https://d396qusza40orc.cloudfront.net/getdata%2Fjeff.jpg" , destfile = "pic.jpg", mode = 'wb' )
-
-img <- readJPEG("pic.jpg", native = TRUE)
-
+url <- "https://d396qusza40orc.cloudfront.net/getdata%2Fjeff.jpg"
+file <- "pic.jpg"
+download.file(url = url, destfile = file, mode = 'wb')
+img <- readJPEG(file, native = TRUE)
 quantile(img,c(0.3,0.8))
 
+
 ## Question 3
+library(data.table)
 url <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FGDP.csv"
-f <- file.path(getwd(), "GDP.csv")
-download.file(url, f)
-dtGDP <- data.table(read.csv(f, skip=4, nrows=215))
+file <- "C:/Data/Coursera/GettingAndCleaningData/GDP.csv"
+download.file(url, file)
+dtGDP <- data.table(read.csv(file, skip=4, nrows=215))
 dtGDP <- dtGDP[X != ""]
 dtGDP <- dtGDP[, list(X, X.1, X.3, X.4)]
 setnames(dtGDP, c("X", "X.1", "X.3", "X.4"), c("CountryCode", "rankingGDP", "Long.Name", "gdp"))
 
 url <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FEDSTATS_Country.csv"
-f <- file.path(getwd(), "EDSTATS_Country.csv")
-download.file(url, f)
-dtEd <- data.table(read.csv(f))
+file <- "C:/Data/Coursera/GettingAndCleaningData/EDSTATS_Country.csv"
+download.file(url, file)
+dtEd <- data.table(read.csv(file))
+
 dt <- merge(dtGDP, dtEd, all=TRUE, by=c("CountryCode"))
 sum(!is.na(unique(dt$rankingGDP)))
 dt[order(rankingGDP, decreasing=TRUE), list(CountryCode, Long.Name.x, Long.Name.y, rankingGDP, gdp)][13]
 
+
 ## Question 4
 dt[, mean(rankingGDP, na.rm = TRUE), by = Income.Group]
+
 
 ## Question 5
 breaks <- quantile(dt$rankingGDP, probs = seq(0, 1, 0.2), na.rm = TRUE)
